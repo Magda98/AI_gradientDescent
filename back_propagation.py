86ff83c5308f826a3ed4a7c73f6aa3f45884b55d
unknown <magdakochman7@gmail.com>
2019-03-28 19:06:56 +0100

    #2

--- back_propagation.py
+++ back_propagation.py
@@ -1,28 +1,36 @@
 import math
 import numpy as np
+from random import sample, shuffle
 import matplotlib.pyplot as plt
+from numba import vectorize, float64, int32, int64, float32
 
-np.seterr('raise')
+@vectorize([int32(int32, int32),
+            int64(int64, int64),
+            float32(float32, float32),
+            float64(float64, float64)])
+def ff(a, b):
+    return a * b
+
 
 def normalization(x, xmin, xmax):
     for idv,val in enumerate(x):
         x[idv] = ((val - xmin)/(xmax-xmin))
     return x
 
+
 def activation(x):
-    B = 1
-    return 1/(1 + (np.e**(-B*x)))
+    return 1/(1 + np.e**(-x))
 
 def derivative(x):
-    return activation(x)*(1-activation(x))
+    z = activation(x)
+    return z*(1 - z)
 
 def weights (tab, weight):
-    return tab * weight
+    return ff(tab ,weight)
 
 def hiddenlayer(tab, neuronsNumber, weight):
     arg = []
     ee = []
-    # wymiary 'weight' to num x 16, neurnsNumber - ilosc neuronow w warstwie ukrytej 
     for i in range(neuronsNumber):
         e = sum(weights(tab, weight[i]))
         ee.append(e)
@@ -30,108 +38,106 @@ def hiddenlayer(tab, neuronsNumber, weight):
     return [arg, ee]
 
 def outlayer(tab, weight):
+    tab = np.asarray(tab)
     x = sum(tab*weight)
     return x
 
 def loss(out, val):
-    return val - out
+    return (val - out)
 
-def gradient(l, weight):
+def error(l, weight):
     return weight * l
 
-def gradient1(g, weight):
-    x =  sum(weight * g)
+def error1(err, weight):
+    x =  sum(weight * err)
     return x
 
-def weightUpdate1(weight, localGradient, tab, e, learningRate):
+def weightUpdate1(weight, errors, arg, fe, learningRate):
     for i, val in enumerate(weight):
         for j in range(val.size):
-            weight[i][j] += learningRate*derivative(e[i])*localGradient[i]*tab[j]
+            weight[i][j] += learningRate*derivative(fe[i])*errors[i]*arg[j]
     return weight
 
-def weightUpdate2(weight, l, y, e, learningRate):
+def weightUpdate2(weight, l, arg, out, learningRate):
     for i in range(weight.size):
-        weight[i] += lr*l*derivative(y)*e[i]
+        weight[i] += learningRate*l*1*arg[i]
     return weight
+
+def testNet(wages, testPn, testTn, neuronsInLayers, layerNum):
+    mse = []
+    testResult = []
+    for i, tab in enumerate(testPn):
+        fe = []
+        arg = []
+        fe_arg = []
+        fe.append(tab)
+        for k in range(layerNum):
+            fe_arg = hiddenlayer(fe[k], neuronsInLayers[k], wages[k])
+            fe.append(fe_arg[0])
+            arg.append(fe_arg[1])
+        y = outlayer(fe[-1], wages[-1])
+        testResult.append(y)
+        arg.append(sum(fe[-1] * wages[-1]))
+        fe.append(y)
+        ls = loss(y, testTn[i])
+        mse.append(0.5*ls**2)
+    return [np.array(mse).max(), testResult]
 
 def neuralNetwork(Pn, Tn, layerNum, neuronsInLayers, epochNum, learningRate, testPn, testTn):
     # wagi dla danej warstwy, 15 - bo tyle jest wejść
-    w = []
-    w.append(np.random.rand( neuronsInLayers[0], 15))
+    wages = []
+    wages.append(np.random.rand( neuronsInLayers[0], 15))
     for i in range(1, layerNum):
-        w.append(np.random.rand(neuronsInLayers[i], neuronsInLayers[i-1] ))
+        wages.append(np.random.rand(neuronsInLayers[i], neuronsInLayers[i-1] ))
     # dla ostatniej warstwy
-    lastl = 0
-    w.append(np.random.rand(neuronsInLayers[-1]))
-    std_dev, maxx = [], []
+    wages.append(np.random.rand(neuronsInLayers[-1]))
+
     for j in range(epochNum):
-        wynik = []
-        ll = []
-        for i, tab in enumerate(Pn):
-            e = []
-            ee = []
-            ddd = []
-            e.append(tab)
+        result = []
+        for i, inData in enumerate(Pn):
+            fe = []
+            arg = []
+            fe_arg = []
+            fe.append(inData)
             for k in range(layerNum):
-                ddd = hiddenlayer(e[k], neuronsInLayers[k], w[k])
-                e.append(ddd[0])
-                ee.append(ddd[1])
+                fe_arg = hiddenlayer(fe[k], neuronsInLayers[k], wages[k])
+                fe.append(fe_arg[0])
+                arg.append(fe_arg[1])
+            output = outlayer(fe[-1], wages[-1])
+            arg.append(sum(fe[-1] * wages[-1]))
+            fe.append(output)
+            ls = loss(output, Tn[i])
+            result.append(output)
 
-            y = outlayer(e[-1], w[-1])
-            ee.append(sum(e[-1] * w[-1]))
-            e.append(y)
-            l = loss(y, Tn[i])
-            # if(l > lastl):
-            #     learningRate -= 0.0005
-            # else:
-            #     learningRate += 0.0005
-            # lastl = l
-            wynik.append(y)
-            ll.append(l)
-            wg =  w[::-1]
-            wl = neuronsInLayers[::-1]
-            g = []
-            g.append(gradient(l, wg[0]))
+            wage_fl =  wages[::-1]
+            nil_fl = neuronsInLayers[::-1]
+            errors = []
+            errors.append(gradient(ls, wage_fl[0]))
             for k in range(1, layerNum):
-                t = wg[k]
-                t = t.transpose()
-                gg = []
-                for p in range(wl[k]):
-                    gg.append(gradient1(g[k-1], t[p]))
-                g.append(np.asarray(gg))
-            g = g[::-1]
+                temp = wage_fl[k]
+                temp = temp.transpose()
+                temp_errors = []
+                for p in range(nil_fl[k]):
+                    temp_errors.append(gradient1(errors[k-1], temp[p]))
+                errors.append(np.asarray(temp_errors))
+            errors = errors[::-1]
             for k in range(layerNum):
-                w[k] = weightUpdate1(w[k], g[k], e[k], ee[k], learningRate)
-            w[layerNum] = weightUpdate2(w[layerNum], l, ee[-1], e[-2], learningRate)
+                wages[k] = weightUpdate1(wages[k], errors[k], fe[k], arg[k], learningRate)
+            wages[layerNum] = weightUpdate2(wages[layerNum], ls, fe[-2], arg[-1], learningRate)
 
-        mx = np.array(ll).max()
-        sd = np.array(ll).std()
-
-        std_dev.append(sd)
-        maxx.append(mx)
-        print(f'Epoka #{j:02d} (MX: {mx:.10f}, SD: {sd:.10f})', end='\r')
-    ww = []
-    lll = []
-    for i, tab in enumerate(testPn):
-        e = []
-        ee = []
-        ddd = []
-        e.append(tab)
-        for k in range(layerNum):
-            ddd = hiddenlayer(e[k], neuronsInLayers[k], w[k])
-            e.append(ddd[0])
-            ee.append(ddd[1])
-
-        y = outlayer(e[-1], w[-1])
-        ee.append(sum(e[-1] * w[-1]))
-        e.append(y)
-        l = loss(y, Tn[i])
-        ww.append(y)
-        lll.append(l)   
-    plt.plot(ww)
+        mse = testNet(wages, testPn, testTn, neuronsInLayers, layerNum)[0]
+        if(mse < 0.3):
+            break
+        print(f'Epoka #{j:02d} mse: {mse:.10f}', end='\r')
+    testResult = testNet(wages, testPn, testTn, neuronsInLayers, layerNum)[1]
+    plt.plot(result)
+    plt.plot(Tn)
+    plt.figure()
+    plt.plot(testResult)
     plt.plot(testTn)
 
 
+#main#
 
 # wczytanie i przygotowanie danych
 testData = []
@@ -140,28 +146,35 @@ with open("zoo.txt") as f:
     data = [list(map(float, x.strip().split(',')[1:])) for x in f]
 
 data = np.array(data).reshape(101,17)
-testData = data[0:30,:]
-data = data[30:101, :]
+data = data.tolist()
+shuffle(data)
+data = np.asarray(data)
+testData = data[0:20, :]
+data = data[20:101, :]
 data=data[np.argsort(data[:,16])]
+testData = np.asarray(testData)
 testData =testData[np.argsort(testData[:,16])]
 data = data.transpose()
 testData = testData.transpose()
-testPn = testData[0:15]
-testTn = testData[16:17][0]
+
+
 Pn = data[0:15]
 Tn = data[16:17][0]
-for x, val in enumerate(Pn):
-    Pn[x]  = normalization(val, np.min(val), np.max(val))
-for x, val in enumerate(testPn):
-    testPn[x]  = normalization(val, np.min(val), np.max(val))
+
+testPn = testData[0:15]
+testTn = testData[16:17][0]
+
+# for x, val in enumerate(Pn):
+Pn[12]  = normalization(Pn[12], np.min(Pn[12]), np.max(Pn[12]))
+# for x, val in enumerate(testPn):
+testPn[12]  = normalization(testPn[12], np.min(testPn[13]), np.max(testPn[13]))
 
 
-lr = 0.5
+lr = 0.05
 Pn = Pn.transpose()
 testPn = testPn.transpose()
 
 
-n = [40, 15]
-neuralNetwork(Pn, Tn, len(n), n, 20, lr, testPn, testTn)
+n = [26, 12, 4]
+neuralNetwork(Pn, Tn, len(n), n, 30000, lr, testPn, testTn)
 plt.show()
-
